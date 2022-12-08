f7983d0fc36e7e7388bf7a0e0b9449c185f674c0
Nagendra Singh <ns284719@gmail.com>
2022-12-09 00:13:11 +0530

    remove duplicate

---
+++ removeduplicate.cpp
@@ -0,0 +1,12 @@
+ int removeDuplicates(vector<int>& arr) {
+       int j=1;
+        for(int i=1;i<arr.size();i++)
+        {
+            if(arr[i]!=arr[i-1])
+            {
+                arr[j]=arr[i];
+                j++;
+            }
+        }
+        return j;
+    }
